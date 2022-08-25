5577c857980f2ac1f0cf2af899aa502d91625def
Jeremy <jeremy@jeremyscott.io>
2022-08-25 13:05:38 +1000

    add test and update series

--- test/ArithmeticSpec.hs
+++ test/ArithmeticSpec.hs
@@ -12,7 +12,17 @@ getEodExample :: EndOfDayData
 getEodExample = createEodWithPriceOf 10 20 30 40
 
 getEodSeries  :: [EndOfDayData]
-getEodSeries = [(createEodWithPriceOf 20 20 20 20), (createEodWithPriceOf 20 20 20 20), (createEodWithPriceOf 40 40 40 40)]
+getEodSeries = [
+  (createEodWithPriceOf 40 40 40 40),
+  (createEodWithPriceOf 40 40 40 40),
+  (createEodWithPriceOf 40 40 40 40),
+  (createEodWithPriceOf 40 40 40 40),
+  (createEodWithPriceOf 40 40 40 40),
+  (createEodWithPriceOf 20 20 20 20),
+  (createEodWithPriceOf 20 20 20 20),
+  (createEodWithPriceOf 20 20 20 20),
+  (createEodWithPriceOf 20 20 20 20),
+  (createEodWithPriceOf 20 20 20 20)]
 
 spec :: Spec
 spec = do
@@ -20,12 +30,14 @@ spec = do
     it "calculates the mean of an end of day data point" $ do
       mean getEodExample `shouldBe` 25
     it "calculates the mean price of a series of end of day data points" $ do
-      mean getEodSeries `shouldBe` 26.666666666666668
+      mean getEodSeries `shouldBe` 30
   describe "Simple Moving Average" $ do
     it "calculates the sma of an end of day series" $ do
-      sma getEodSeries `shouldBe` 26.666666666666668
+      sma getEodSeries `shouldBe` 30
   describe "Exponential Moving Average" $ do
     it "returns Nothing when there are insufficient Elements" $ do
-      ema getEodSeries 2 `shouldBe` Nothing
+      ema getEodSeries 10 `shouldBe` Nothing
     it "does not return Nothing when there are sufficient Elements" $ do
       ema getEodSeries 1 `shouldNotBe` Nothing
+    it "calculates the EMA given a series of sufficient length" $ do
+      ema getEodSeries 5 `shouldBe` Just 10
