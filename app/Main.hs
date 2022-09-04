1d66e89ee41d561bc8291c284fb28244c8e80f16
Jeremy Scott <jeremy@jeremyscott.io>
2022-09-04 10:06:27 +1000

    add generic entries for CLI

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,29 @@
 module Main (main) where
+import System.Environment
+import Data.List
+import Data.Maybe
+
+data Action = ExecuteAction | BuyAction | SellAction
 
 main :: IO ()
-main = printDefault
+main = do
+  args <- getArgs
+  if (length args) == 1 || (length args) == 3
+    then performAction $ determineAction (args!!0)
+    else printDefault
+
+performAction :: Maybe Action -> IO ()
+performAction Nothing = printDefault
+performAction (Just ExecuteAction) = putStrLn "ExecuteAction"
+performAction (Just BuyAction) = putStrLn "BuyAction"
+performAction (Just SellAction) = putStrLn "SellAction"
+
+
+determineAction :: String -> Maybe Action
+determineAction arg | arg == "-e" = Just ExecuteAction
+                    | arg == "-b" = Just BuyAction
+                    | arg == "-s" = Just SellAction
+                    | otherwise = Nothing
 
 printDefault :: IO ()
 printDefault = do
