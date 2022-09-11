cb3821ac6cd3bd709486e43a4987ef519ebd5301
Jeremy <jeremy@jeremyscott.io>
2022-09-11 10:24:05 +1000

    add yahoo api client

--- app/YahooClient.hs
+++ app/YahooClient.hs
@@ -1,20 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module YahooClient where
 --https://query1.finance.yahoo.com/v8/finance/chart/NAB.AX?region=AU&lang=en-AU&includePrePost=false&interval=1d&useYfid=true&range=max&corsDomain=au.finance.yahoo.com&.tsrc=finance
 
+import Control.Monad.IO.Class
 import Network.HTTP.Req
-
 import Data.Aeson
 import Data.Maybe
 import Data.Text
 
+import Prelude
+
+import Data.List.Split
+import Data.List.Utils (replace)
+
 import Asset
 
 getAsset :: AsxListedCompany -> IO Asset
-getAsset company = do
-      response <- runReq defaultHttpConfig $ req
-                    GET
-                    (https "query1.finance.yahoo.com" /: "v4" /: "finance" /: "chart" /: pack $ (companyCode company) + ".AX")
-                    NoReqBody
-                    jsonResponse
-                    mempty
-      return $ Asset 0 (companyCode company) "ASX" "" []
+getAsset company = runReq defaultHttpConfig $ do
+  response <- req
+                GET
+                (https "query1.finance.yahoo.com" /: "v4" /: "finance" /: "chart" /: (pack $ (companyCode company) ++ ".AX"))
+                NoReqBody
+                jsonResponse
+                mempty
+  liftIO $ print (responseBody response :: Value)
+  return $ Asset 0 (companyCode company) "ASX" "" []
